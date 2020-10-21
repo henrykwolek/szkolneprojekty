0c176f2bec2931aafc33e44ee07ae47d0c2befef
henrykwolek <henrykwolek15@gmail.com>
2020-10-21 08:21:01 +0200

    first commit

---
+++ polekola.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    double r;
+    const double pi = 3.14159;
+    double pole;
+    cout << "Podaj promień koła";
+    cin >> r;
+    pole = pi * r * r;
+    cout << pole;
+
+}   
